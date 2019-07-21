5769fd5cc5bbb58a17a61103ffc313bb91d8234f
cassandra riley <kimaniwalker@gmail.com>
2019-07-20 19:41:04 -0500

    Added Login Route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const apiRouter = require('./routes');
+
+
+const testRouter = require('../server/routes/test');
+const loginRouter = require('../server/routes/login');
 
 const app = express();
-
 app.use(express.json());
 
-app.use('/api/chirps', apiRouter);
+app.use('/api/test', testRouter);
+app.use('/api/login', loginRouter);
+
 
 app.listen(process.env.PORT || '3000' , () => {
     console.log(`Server Is Up & Running on port: ${process.env.PORT || '3000'}`);
